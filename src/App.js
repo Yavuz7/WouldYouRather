9d60114c1112d45b390636c6a695a3f06726b42e
Yavuz7 <yavuzyurtseven1@gmail.com>
2023-02-02 23:13:19 -0500

    Added color changing with div clicking, and text over div

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 
 function App() {
   return (
-   <div>Hi hi</div>
+    <div></div>
   );
 }
 
--- src/ChoicePanel.js
+++ src/ChoicePanel.js
@@ -1,15 +1,25 @@
 import { useState } from 'react';
 import './ChoicePanel.css';
 
+
+
 function ChoicePanel(){
-    function selectionHandler(event){
-      
+    const [color,setColor] = useState(true);
+    const [color2,setColor2] = useState(true);
+
+    function colorHandler(prop){
+       if( prop === "banana"){
+        setColor(!color);
+       }
+       else if( prop === "banana2"){
+        setColor2(!color2);
+       }
     }
    return(
     <div>
-    <div id="question"> Hello!</div>
-    <div id ="banana" className="banana-zone" onClick={selectionHandler}/>
-    <div id="banana2" className="banana-zone b2" onClick={selectionHandler}/>
+    <div className='question'><p>Hello!</p> </div>
+    <div  className="banana-zone b1" style={{ background: color2? 'red':'lightblue'}} onClick={() => colorHandler('banana')}/>
+    <div className="banana-zone b2" style={{ background: color? 'blue':'pink'}} onClick={() => colorHandler('banana2')}/>
     </div>
    )
 }

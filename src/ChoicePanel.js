936591f7394f200cb7c83d1cee7bf282bed965d8
Yavuz7 <yavuzyurtseven1@gmail.com>
2023-02-03 17:42:17 -0500

    Maybe code prettier and simpler

--- src/ChoicePanel.js
+++ src/ChoicePanel.js
@@ -1,23 +1,29 @@
-import { useState } from 'react';
-import './ChoicePanel.css';
+import { useState } from "react";
+import "./ChoicePanel.css";
 
-export default function ChoicePanel({leftPercentage,colorUnselected,colorSelected}){
+export default function ChoicePanel({
+  leftPercentage,
+  colorUnselected,
+  colorSelected,
+}) {
+  const [select, setSelect] = useState(true);
 
-    const [select,setSelect] = useState(true);
-
-    const selectedStyle = {
-      background: colorUnselected,
-      width: "50%",
-      zIndex: "2",
-      left: leftPercentage
-    }
-    const unSelectedStyle = {
-      background: colorSelected,
-      width: "100%",
-      zIndex: "4"
-    }
-   return(
-    <div className="banana-zone b1" style= {select? selectedStyle:unSelectedStyle} onClick={() => setSelect(!select)}/>
-   )
+  const selectedStyle = {
+    background: colorUnselected,
+    width: "50%",
+    zIndex: "2",
+    left: leftPercentage,
+  };
+  const unSelectedStyle = {
+    background: colorSelected,
+    width: "100%",
+    zIndex: "4",
+  };
+  return (
+    <div
+      className="banana-zone b1"
+      style={select ? selectedStyle : unSelectedStyle}
+      onClick={() => setSelect(!select)}
+    />
+  );
 }
-
--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
-import  ReactDOM  from "react-dom/client";
+import ReactDOM from "react-dom/client";
 import ChoicePanel from "./ChoicePanel";
 
-
-<div id="root"></div>
-const root = ReactDOM.createRoot(
-    document.getElementById('root')
-    );
-    const thePage = (     
-            <div id ="mainstuff">  
-              {/*First Panel is Red Second Is Blue*/}
-                <ChoicePanel leftPercentage={'0%'} colorUnselected={'red'} colorSelected={'pink'} />
-                <ChoicePanel leftPercentage={'50%'} colorUnselected={'blue'} colorSelected={'lightblue'}/>
-            </div>
-    )
+<div id="root"></div>;
+const root = ReactDOM.createRoot(document.getElementById("root"));
+const thePage = (
+  <div id="mainstuff">
+    {/*First Panel is Red Second Is Blue*/}
+    <ChoicePanel
+      leftPercentage={"0%"}
+      colorUnselected={"red"} 
+      colorSelected={"pink"}
+    />
+    <ChoicePanel
+      leftPercentage={"50%"}
+      colorUnselected={"blue"}
+      colorSelected={"lightblue"}
+    />
+  </div>
+);
 root.render(thePage);

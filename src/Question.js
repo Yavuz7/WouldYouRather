51ea1bea5874c6f0cda6d82c05ba559d4599e90d
Yavuz7 <yavuzyurtseven1@gmail.com>
2023-02-04 15:26:57 -0500

    Installed Axios, and Have Working Api Calls

--- src/Question.js
+++ src/Question.js
@@ -1,10 +1,28 @@
+import { useState, useEffect } from "react";
+import "./Question.css";
+import axios from "axios";
 
+//Api From : https://replit.com/@AbaanShanid :3
+//Code Repurposed : https://www.freecodecamp.org/news/how-to-use-axios-with-react/#how-to-set-up-axios-with-react
+const baseUrl = "https://would-you-rather-api.abaanshanid.repl.co/"
 
 export default function Question() {
     
-    return ( 
-    <div>
+    const[post,setPost] = useState(null);
+   
 
+    useEffect( () => {
+        axios.get(baseUrl).then((response) => {
+            setPost(response.data);
+        })
+    },[]);
+
+    if (!post) return null;
+
+    return ( 
+        
+    <div className="question">
+        <p>{post.data}</p>
 
 
     </div>
--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import ReactDOM from "react-dom/client";
 import ChoicePanel from "./ChoicePanel";
+import Question from "./Question";
 
 <div id="root"></div>;
 const root = ReactDOM.createRoot(document.getElementById("root"));
 const thePage = (
   <div id="mainstuff">
+   
     {/*First Panel is Red Second Is Blue*/}
     <ChoicePanel
       leftPercentage={"0%"}
@@ -18,6 +20,7 @@ const thePage = (
       colorSelected={"lightblue"}
       panelAnimationSet = {"blueSet"}
     />
+     <Question/>
   </div>
 );
 root.render(thePage);

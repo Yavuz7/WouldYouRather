73b989e16680c34a5d3478fcf7aec67eb63502db
Yavuz7 <yavuzyurtseven1@gmail.com>
2023-02-04 17:24:50 -0500

    Added Text Splitting and adjusted position of text

--- src/Question.js
+++ src/Question.js
@@ -4,29 +4,24 @@ import axios from "axios";
 
 //Api From : https://replit.com/@AbaanShanid :3
 //Code Repurposed : https://www.freecodecamp.org/news/how-to-use-axios-with-react/#how-to-set-up-axios-with-react
-const baseUrl = "https://would-you-rather-api.abaanshanid.repl.co/"
+const baseUrl = "https://would-you-rather-api.abaanshanid.repl.co/";
 
 export default function Question() {
-    
-    const[post,setPost] = useState(null);
-   
+  const [post, setPost] = useState(null);
 
-    useEffect( () => {
-        axios.get(baseUrl).then((response) => {
-            setPost(response.data);
-        })
-    },[]);
+  useEffect(() => {
+    axios.get(baseUrl).then((response) => {
+      setPost(response.data);
+    });
+  }, []);
 
-    if (!post) return null;
+  if (!post) return null;
 
-    return ( 
-        
-    <div className="question">
-        <p>{post.data}</p>
-
-
+  const splitQuestion = post.data.split(" or ");
+  return (
+    <div className = "questions">
+      <p className="question" style = {{left: '25%', maxWidth: '350px'}}>{splitQuestion[0] }</p>
+      <p className="question" style = {{right: '25%', maxWidth: '350px'}}>{splitQuestion[1]}</p>
     </div>
-    )
-       
-    
+  );
 }

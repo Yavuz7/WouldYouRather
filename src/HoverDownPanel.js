121818264e305f92caef9d9eb4a3c2629587a1c9
Yavuz7 <yavuzyurtseven1@gmail.com>
2023-02-18 19:33:44 -0500

    Added Imgs, Attached Them To HoverDown, Working On Css

--- src/HoverDownPanel.js
+++ src/HoverDownPanel.js
@@ -1,14 +1,17 @@
 import "./HoverDownPanel.css";
+import gitHubIcon from "./images/githubicon.png"
+import aboutmeicon from "./images/aboutmeicon.png"
+import aboutsiteicon from "./images/aboutsiteicon.png"
 
 export default function HoverDownPanel() {
     return(
     <>
         <div className="hoverDownGrid">
         <section className="gridContent">           
-            <span>Test</span>
-            <span>Test2</span>
-            <span>Test3</span>
-            <span>ArrowThatYouHoverOver</span>
+            <span><img src={gitHubIcon} alt="GitHubIcon"></img>GitHub</span>
+            <span><img src={aboutmeicon} alt="AboutSiteIcon"></img>About Me</span>
+            <span><img src={aboutsiteicon} alt="AboutMeIcon"></img>About Site</span>
+            <span>More Information</span>
         </section>
 
         </div>

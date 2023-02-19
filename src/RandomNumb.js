d15dcf6aa5334467e010e5eee24ed3803b8e274c
Yavuz7 <yavuzyurtseven1@gmail.com>
2023-02-19 14:49:59 -0500

    Made Text More Responsive

--- src/RandomNumb.js
+++ src/RandomNumb.js
@@ -3,11 +3,13 @@ import "./Question.css";
 export default function RandomNumb({Variable}) {
   let numbey = Variable;
   let numbey2 = 100 - numbey;
-
+   const styleWidth = '10vw';  
   return (
      <div id="Percentage-Numbers" className="questions">
-        <span className="question" style = {{left: '12%'}}>{numbey}% Chose</span>
-        <span className = "question" style = {{right: '12%'}}>{numbey2}% Chose</span> 
+        <span className="question" style = {{left: styleWidth}}>{numbey}%</span>
+        <text className = "question" style = {{left: styleWidth}}>Of People Chose</text>
+        <span className = "question" style = {{right: styleWidth}}>{numbey2}%</span>
+        <text className = "question" style = {{right: styleWidth}}>Of People Chose</text> 
      </div>
   );
 }

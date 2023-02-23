65a0ea78d81dc0aebeeed424fd544401c77929f2
Yavuz7 <yavuzyurtseven1@gmail.com>
2023-02-23 17:06:45 -0500

    Finished Modal Functionality, works wonderfully!

--- src/HoverDownPanel.js
+++ src/HoverDownPanel.js
@@ -1,20 +1,61 @@
 import "./HoverDownPanel.css";
+import { useState } from "react";
 import gitHubIcon from "./images/githubicon.png"
 import aboutmeicon from "./images/aboutmeicon.png"
 import aboutsiteicon from "./images/aboutsiteicon.png"
+import Modal from "./Modal";
+
 
 export default function HoverDownPanel() {
+    const [modalActive,setModalActive] = useState(0);
+    const [modalInfo,setModalInfo] = useState({title:"",info:"",link:""});
+    
+    function changeModal(modalNumber)
+    {
+        switch(modalNumber)
+        {
+            case 1:
+                {
+                    setModalInfo({
+                        title:"GitHub",
+                        info :"The link below will bring you to the github in a new tab!",
+                        link: "https://github.com/Yavuz7/WouldYouRather"
+                    });                
+                    setModalActive(1);
+                    return;
+                }
+            case 2:
+            {
+                setModalInfo({
+                    title:"About Me",
+                    info :"My name is Yavuz, I'm a IT major with a concentration on multimedia. I'm currently focusing on learning React and this is my first project using it!"
+                });                
+                setModalActive(1);
+                return;
+            }
+            case 3:
+            {
+                setModalInfo({
+                    title:"About Site",
+                    info :"This site is a Would You Rather game built using react. It makes extensive use of some of the hooks react has to offer, and even uses an api I found online:",
+                    link:"https://replit.com/@AbaanShanid"
+                });                
+                setModalActive(1);
+                return;
+            }
+        }
+    }
     return(
     <>
         <div className="hoverDownGrid">
         <section className="gridContent">           
-            <span><a href ="https://github.com/Yavuz7/WouldYouRather" target="_blank" rel="noreferrer"><img src={gitHubIcon} alt="GitHubIcon"></img></a>GitHub</span>
-            <span><img src={aboutmeicon} alt="AboutSiteIcon"></img>About Me</span>
-            <span><img src={aboutsiteicon} alt="AboutMeIcon" href=""></img>About Site</span>
+            <span onClick={() => changeModal(1)}><img src={gitHubIcon} alt="GitHubIcon"></img>GitHub</span>
+            <span onClick={() => changeModal(2)}><img src={aboutmeicon} alt="AboutSiteIcon"></img>About Me</span>
+            <span onClick={() => changeModal(3)}><img src={aboutsiteicon} alt="AboutMeIcon" href=""></img>About Site</span>
             <span>More Information</span>
         </section>
-
         </div>
+        <Modal onClose={() => setModalActive(0)} modalActive={modalActive} title = {modalInfo.title} info = {modalInfo.info} link={modalInfo.link}/>
     </>
     )
 }
--- src/Modal.js
+++ src/Modal.js
@@ -1,15 +1,23 @@
-import "./Modal.css"
+import "./Modal.css";
 
-export default function Modal(){
-
-return(
-    <div className="modalStuff">
+export default function Modal(props) {
+  {
+    /*Code Repurposed from: https://medium.com/tinyso/how-to-create-a-modal-component-in-react-from-basic-to-advanced-a3357a2a716a */
+  }
+  if (!props.modalActive) {
+    return null;
+  }
+  return (
+    <>
+    <div className="modalBackground" onClick={props.onClose}/>
+      <div className="modalStuff">
         <div className="modalContent">
-        <button className="closeModalButton">X</button>
-        <h1>Title Here</h1>
-        <p>Info Here</p>
-       
+          <button className="closeModalButton" onClick={props.onClose}>  X </button>
+          <h1>{props.title}</h1>
+          <p>{props.info}</p>
+          <a href={props.link} target="_blank">{props.link}</a>
         </div>
-    </div>
-);
+      </div>
+    </>
+  );
 }
